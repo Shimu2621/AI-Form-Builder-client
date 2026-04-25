ce3aa1e8beec553511a9b34884055d5723fb0b04
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-24 23:02:21 -0400

    update file

--- src/app/(mainlayout))/support/page.tsx
+++ src/app/(mainlayout))/support/page.tsx
@@ -4,7 +4,8 @@ import SupportWrapper from "./SupportWrapper";
 
 export const metadata: Metadata = {
   title: "Support | AI Form Builder",
-  description: "Get in touch with our support team or visit our community forums for help and guidance.",
+  description:
+    "Get in touch with our support teams or visit our community forums for help and guidance.",
 };
 
 export default function Page() {
