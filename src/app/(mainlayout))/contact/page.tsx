ccb216be0bd1d6edfe681af45a19b8596ff51d25
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-24 23:12:47 -0400

    update file

--- src/app/(mainlayout))/contact/page.tsx
+++ src/app/(mainlayout))/contact/page.tsx
@@ -5,7 +5,7 @@ import ContactWrapper from "./ContactWrapper";
 export const metadata: Metadata = {
   title: "Contact Us | AI Form Builder",
   description:
-    "We are here to help you with any questions you have. Reach out and we'll respond as soon as possible.",
+    "We are here to help you with any questions you have. Reach out and we'll response as soon as possible.",
 };
 
 export default function Page() {
