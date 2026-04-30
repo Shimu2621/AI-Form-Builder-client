1bce7cb738816366b8bd4d11de0750842412d5d5
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-29 23:31:50 -0400

    update file

--- src/app/(mainlayout))/status/page.tsx
+++ src/app/(mainlayout))/status/page.tsx
@@ -5,7 +5,7 @@ import StatusWrapper from "./StatusWrapper";
 export const metadata: Metadata = {
   title: "System Status | AI Form Builder",
   description:
-    "Check the current status of our services and APIs in real-time. Get insights into uptime, response time, and recent incidents.",
+    "Check the current status of our services and APIs in real-time. Get insights into uptime, response time, and recent incident.",
 };
 
 export default function Page() {
