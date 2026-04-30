2782ff154596d3dc481c9692bf2d2572754b0c8f
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-29 23:32:51 -0400

    update file

--- src/app/(mainlayout))/status/SchemaMarkup.tsx
+++ src/app/(mainlayout))/status/SchemaMarkup.tsx
@@ -25,7 +25,7 @@ const SchemaMarkup = () => {
           item: {
             "@type": "Service",
             name: "Database",
-            description: "Stores all user and form data.",
+            description: "Store all user and form data.",
           },
         },
         {
