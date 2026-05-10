c5fdad382820a84238fb7d570c61250c36f51fde
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-09 23:57:35 -0400

    update page

--- src/app/(mainlayout))/page.tsx
+++ src/app/(mainlayout))/page.tsx
@@ -4,7 +4,7 @@ import HomeWrapper from "./HomeWrapper";
 export const metadata: Metadata = {
   title: "AI Form Builder | Create Custom Forms with AI",
   description:
-    "Build powerful, custom forms for your business with our AI-powered form builder. Create job applications, surveys, and more using natural language.",
+    "Build powerful, custom forms for your business with our AI-powered form builder. Create job applications, surveys, and more using natural languages.",
 };
 
 export default function Page() {
