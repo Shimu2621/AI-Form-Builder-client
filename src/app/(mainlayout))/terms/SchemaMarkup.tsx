1f794efc653dec552f4f62d3408c17421d7942a6
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-11 23:04:51 -0400

    update file

--- src/app/(mainlayout))/terms/SchemaMarkup.tsx
+++ src/app/(mainlayout))/terms/SchemaMarkup.tsx
@@ -57,7 +57,7 @@ const SchemaMarkup = () => {
       {
         "@type": "WebPageElement",
         name: "Changes to Terms",
-        text: "We reserve the right, at our sole discretion, to modify or replace these Term at any time.",
+        text: "We reserve the right, at our sole discretion, to modify, update, or replace these Terms at any time without prior notice.",
       },
     ],
   };
