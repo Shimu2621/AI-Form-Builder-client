4a1b075eea749616b2f33420638183f2a32404ca
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-12 23:51:29 -0400

    update file

--- src/app/(mainlayout))/company/company-client.tsx
+++ src/app/(mainlayout))/company/company-client.tsx
@@ -202,7 +202,7 @@ export default function CompanyPage() {
               <p className="text-xl text-gray-600 dark:text-gray-300 leading-relaxed">
                 Our journey is just beginning, and we&apos;re excited to
                 continue innovating and empowering creators everywhere to bring
-                their ideas to lifes.
+                their ideas to life.
               </p>
             </div>
           </motion.div>
