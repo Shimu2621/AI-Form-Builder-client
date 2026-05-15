c101360b947acaf8b9c12f6efb678c080fce755f
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-14 23:52:34 -0400

    update file

--- src/app/(mainlayout))/company/company-client.tsx
+++ src/app/(mainlayout))/company/company-client.tsx
@@ -124,7 +124,7 @@ export default function CompanyPage() {
       </section>
 
       {/* Values Section */}
-      <section className="py-20 px-8s">
+      <section className="py-20 px-8">
         <div className="max-w-6xl mx-auto">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
