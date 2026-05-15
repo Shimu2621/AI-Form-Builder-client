15f1dcd74e21eba784878027c09502b29d1be6ae
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-14 23:46:11 -0400

    update file

--- src/app/(mainlayout))/company/company-client.tsx
+++ src/app/(mainlayout))/company/company-client.tsx
@@ -24,7 +24,7 @@ export default function CompanyPage() {
       icon: Users,
       title: "Community",
       description:
-        "Building a supportive ecosystem where creator can thrive and collaborate.",
+        "Building a supportive ecosystem where creators can thrive and collaborate.",
     },
     {
       icon: Award,
